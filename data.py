bfeaf4fd1022b383ef80582ec283b5e179f1dbec
Lenny <lenny@fedora.fritz.box>
2026-04-28 21:11:38 +0200

    feat: comments added

--- data.py
+++ data.py
@@ -5,26 +5,31 @@ import torchvision.transforms as T
 
 
 def load_data(partition_id, batch_size):
+    #get dataset partition
     fds = FederatedDataset(
         dataset="flwrlabs/femnist",
         partitioners={"train": NaturalIdPartitioner(partition_by="writer_id")}
     )
     partition = fds.load_partition(partition_id=partition_id)
 
+    #describe transform operation on img
     transform = T.Compose([
         T.PILToTensor(),
         T.ConvertImageDtype(float)
     ])
 
+    #apply transform on all PIL in input batch
     def img_to_tensor(batch):
         images = []
-        for img in batch["image"]:
+        for img in batch["imaged"]:
             images.append(transform(img))
         batch["image"] = images
         return batch
 
+    #apply on partition
     partition = partition.with_transform(img_to_tensor)
 
+    #feed transformed partition in dataloader and return it
     dataloader = DataLoader(
         partition,
         batch_size=batch_size,
